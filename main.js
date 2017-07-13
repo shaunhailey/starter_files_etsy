b56b840611d4c8eb8c5af056608f42684cfb8e80
shaunhailey <ediblelysol@gmail.com>
2017-07-13 15:23:45 -0400

    no 2 done

--- main.js
+++ main.js
@@ -3,50 +3,45 @@
 // Open your `index.html` page in your browser and you can verify that the following
 // is properly working. The `data` variable is an array with 25 items in it
 
-
-
 // 1: Show me how to calculate the average price of all items.
 let length = data.length
 let sum = 0
 
-function question1 () {
- for (let i=0; i < length; i++){
-   sum += data[i].price
- }
- console.log(sum/length)
+function question1() {
+  for (let i = 0; i < length; i++) {
+    sum += data[i].price
+  }
+  console.log(sum / length)
 }
-
-
-
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-function question2 () {
-  // Answer:
+
+function question2() {
+  for (let i = 0; i < length; i++) {
+    if (data[i].price >= 14.0 && data[i].price <= 18.0) {
+      console.log(data[i].title)
+    }
+  }
 }
-
-
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
-function question3 () {
+function question3() {
   // Answer:
 }
-
 
 // 4: Display a list of all items who are made of wood.
-function question4 () {
+function question4() {
   // Answer:
 }
-
 
 // 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
-function question5 () {
+function question5() {
   // Answer:
 }
 
-
 // 6: How many items were made by their sellers?
 // Answer:
-function question6 () {
+function question6() {
   // Answer:
 }
